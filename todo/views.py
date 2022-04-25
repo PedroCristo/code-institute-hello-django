4bfcc229ab4fc87cb2d31e31ad9bbd4e33dcee71
Pedro Cristo <pcristo35@gmail.com>
2022-04-25 12:57:30 +0000

    Add toggle and delete code

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,6 @@
-
 # We use the HttpResponse if we write a string in the function
 # from django.shortcuts import render, HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Item
 from .forms import itemForm
 
@@ -14,6 +13,7 @@ def get_todo_nav(request):
     }
     return render(request, 'todo/todo_list.html', context)
 
+
 def add_item(request):
     if request.method == 'POST':
         form = itemForm(request.POST)
@@ -24,6 +24,30 @@ def add_item(request):
     context = {
             'form': form
     }
-
     return render(request, 'todo/add_item.html', context)
 
+
+def edit_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    if request.method == 'POST':
+        form = itemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_nav')
+    form = itemForm(instance=item)
+    context = {
+            'form': form
+    }
+    return render(request, 'todo/edit_item.html', context)
+
+
+def toggle_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.done = not item.done
+    item.save()
+    return redirect('get_todo_nav')
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.delete()
+    return redirect('get_todo_nav')
